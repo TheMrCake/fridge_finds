94b477c77fd16392593f7ac29f549b19adcc49f1
TheMrCake <antonlichota@gmail.com>
2025-12-17 16:02:39 -0400

    Move paths to file

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,5 @@
-from pathlib import Path
 from flask import Flask
 
-PROJECT_ROOT_DIR = Path(__file__).resolve().parent.parent
-
-GLEAM_DIST_DIR = PROJECT_ROOT_DIR / "frontend" / "dist"
 
 print(GLEAM_DIST_DIR)
 
