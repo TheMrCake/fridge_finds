dd913452520002a73b484bd29c9f05bf7a437077
TheMrCake <antonlichota@gmail.com>
2025-12-19 15:22:15 -0400

    Use correct syntax in schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,35 +1,36 @@
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    username TEXT UNIQUE NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
     password_hash TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
 
     author_id INTEGER NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES users (id),
-
-    title TEXT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
     instructions TEXT NOT NULL,
     prep_time_mins INTEGER,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (author_id) REFERENCES users (id)
 );
 
 DROP TABLE IF EXISTS ingredients;
 CREATE TABLE ingredients (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT UNIQUE NOT NULL
 );
 
 DROP TABLE IF EXISTS recipe_ingredients;
 CREATE TABLE recipe_ingredients (
     recipe_id INTEGER NOT NULL,
-    FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE,
-
     ingredient_id INTEGER NOT NULL,
+
+    FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE,
     FOREIGN KEY (ingredient_id) REFERENCES ingredients (id)
 );
